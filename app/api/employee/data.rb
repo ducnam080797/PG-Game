715289abe3ff27661c51ea47b182fbe96cb81b48
dzungnguyen <dung.nguyentien@powergatesoftware.com>
2020-03-30 10:21:33 +0700

    Api_test_303

---
+++ app/api/employee/data.rb
@@ -0,0 +1,44 @@
+module Employee
+    class Data < Grape::API
+  
+      resource :employee_data do
+        desc "List all Employee"
+        get do
+          EmpData.all
+        end
+
+        desc "create a new employee"
+        params do
+          requires :name, type: String
+          requires :address, type:String
+          requires :age, type:Integer
+        end
+        post do
+          EmpData.create!({
+            name:params[:name],
+            address:params[:address],
+            age:params[:age]
+          })
+        end
+
+        desc "update an employee address"
+        params do
+          requires :id, type: String
+          requires :address, type:String
+        end
+        put ':id' do
+          EmpData.find(params[:id]).update({
+            address:params[:address]
+          })
+        end
+
+        desc "delete an employee"
+        params do
+          requires :id, type: String
+        end
+        delete ':id' do
+          EmpData.find(params[:id]).destroy!
+        end
+      end
+    end
+  end
